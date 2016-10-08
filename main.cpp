ad0597dac2eed0f66f87f63c3d9bdf3bba21830c
Mateusz Marzęcki <mateusz.marzecki@gmail.com>
2016-10-09 00:14:07 +0200

    cs
    cs

--- main.cpp
+++ main.cpp
@@ -12,11 +12,11 @@ int main() {
 	
 	scanf("%f %f", &helper1, &helper2);
     
-	helper1*=10; // helps cast weight of first knapsack to int
-	helper2*=10; // helps cast weight of second knapsack to int
+	helper1 *= 10; // helps cast weight of first knapsack to int
+	helper2 *= 10; // helps cast weight of second knapsack to int
     
-	s1 = int(helper1+0.5);	//http://www.cplusplus.com/forum/beginner/33018/
-	s2 = int(helper2+0.5);
+	s1 = int(helper1 + 0.5);	//http://www.cplusplus.com/forum/beginner/33018/
+	s2 = int(helper2 + 0.5);
 	    
 	int **knapsack = new int *[s1 + 1];
 		for (int a = 0; a < s1 + 1; ++a) {
@@ -26,12 +26,12 @@ int main() {
 			}
 		}
 		
-	scanf("%d",&n);
+	scanf("%d", &n);
     	// filing knapsack
 	for (int i = 0; i < n; i++) {
         scanf("%f %d", &helper3, &value);
-        helper3*=10;
-        weight = int(helper3+0.5); // same as above
+        helper3 *= 10;
+        weight = int(helper3 + 0.5); // same as above
 	// need to fill up all the table cannot stop if one sack is full because item might fit in other
         for (int weight1 = s1; weight1 >= 0; weight1--) { // weight of first knapsack
 		for (int weight2 = s2; weight2 >= 0; weight2--) { // weight of second knapsack
@@ -47,9 +47,9 @@ int main() {
 			}
 					
 			knapsack[weight1][weight2] = max(
-				knapsack[weight1][weight2] // we have best option
-				,value1 // put into sack one
-				,value2 // put into sack two
+				knapsack[weight1][weight2], // we have best option
+				value1, // put into sack one
+				value2 // put into sack two
 			);
             }
         }
